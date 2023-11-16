450806482e037c556e4d6f0ab74ae1d2372cdd2e
Famtosha <artem_kazakovtsev@mail.ru>
2023-11-16 17:11:23 +0500

    Updated UI

--- Assets/Source/Scripts/ECS/Configs/CarDefinition.cs
+++ Assets/Source/Scripts/ECS/Configs/CarDefinition.cs
@@ -7,8 +7,8 @@ public sealed class CarDefinition : ScriptableObject
     [field: SerializeField, ShowAssetPreview] public CarComponent prefab { get; private set; }
     [field: SerializeField] public Color color { get; private set; }
     [field: SerializeField] public string displayName { get; private set; }
-    [field: SerializeField] public GiftDefinition gift { get; private set; }
+    [field: SerializeField, Expandable] public GiftDefinition gift { get; private set; }
 
-    [field: SerializeField] public Sprite leaderSprite { get; private set; }
-    [field: SerializeField] public Sprite memeberSprite { get; private set; }
+    [field: SerializeField, ShowAssetPreview] public Sprite leaderSprite { get; private set; }
+    [field: SerializeField, ShowAssetPreview] public Sprite memeberSprite { get; private set; }
 }
